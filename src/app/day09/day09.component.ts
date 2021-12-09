ab79f2d6201f13fd8a9f0785365e6ddaceb279ff
Dorus van Weelden <dorus@bit-academy.nl>
2021-12-09 11:18:01 +0100

    Minor change in executing time display, also .,/' d a y 0 9 '\,.

---
+++ src/app/day09/day09.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-day09',
+  templateUrl: './day09.component.html',
+  styleUrls: ['./day09.component.css']
+})
+export class Day09Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
